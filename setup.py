5ecc5b7014060213d35156304c577915e6ae5e92
Matthew Greer <mgreer906@gmail.com>
2019-10-04 15:01:41 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as f:
+	long_description = f.read()
+
+setuptools.setup(
+	name='subseeker',  
+	version='2.0',
+	author="Matthew Greer",
+	author_email="dfc302@protonmail.com",
+	description="A sub enumeration tool.",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	url="https://github.com/DFC302/subseeker",
+	packages=setuptools.find_packages('subseeker'),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+     ],
+)

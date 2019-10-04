deee92b1411dbd55c48c7903b551d669ccd1e873
Matthew Greer <mgreer906@gmail.com>
2019-10-04 15:05:03 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
 	long_description=long_description,
 	long_description_content_type="text/markdown",
 	url="https://github.com/DFC302/subseeker",
-	packages=setuptools.find_packages('subseeker'),
+	packages=setuptools.find_packages('main'),
 	classifiers=[
 		"Programming Language :: Python :: 3",
 		"License :: OSI Approved :: MIT License",
